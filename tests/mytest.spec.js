93c1c5100fff447c33801ed6525845a594dc07b3
Mittal <sanchit.mittal@mmc.com>
2025-11-19 21:28:35 +1100

    safe commit

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -1,17 +1,21 @@
 const {test} = require("@playwright/test")
 const {POManager} = require('../pageobjects/POManager.page.js')
+const dataSetArray = require('../Dataset.json');
+
 
 test('end to end test', async ({ page }) => {
     
     const poManager = new POManager(page);
-    const username = "rahulshettyacademy ";
-    const password = "learning";
+
+    const field = dataSetArray.Student[0];
+    // const username = "rahulshettyacademy ";
+    // const password = "learning";
     
     
     const myTestnew = poManager.getObject(); 
 
     await myTestnew.goTo();
-    await myTestnew.validLogin(username,password)
+    await myTestnew.validLogin(field)
 
-    await page.pause(10000)
+    await page.pause()
 })
