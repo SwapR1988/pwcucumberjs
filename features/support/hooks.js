8ccfd29f4885c91e88355791edee2aac1c8f0842
SwapR1988 <swapnendras@gmail.com>
2025-12-02 11:39:48 +0530

    yaml update

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,17 +1,20 @@
 const { chromium } = require('playwright');
 const { POManager } = require('../../pageobjects/POManager.page.js');
 const { Before, After, BeforeStep, AfterStep } = require('@cucumber/cucumber');
-
+const path = require('path')
+const fs = require('fs')
 
 Before(async function(){
     browser = await chromium.launch({ headless: false });
     context = await browser.newContext();
     this.page = await context.newPage();
     this.poManager = new POManager(this.page);
+    await genericData();
 })
 
 BeforeStep(async function(){
     await this.page.screenshot({path: 'BeforeStep.png', fullpage:true})
+    
 })
 
 // AfterStep(async function(result){
@@ -24,3 +27,13 @@ BeforeStep(async function(){
 After(async function(){
     console.log("Completed")
 })
+
+let gendata;
+const genericData=()=>{
+    const filePath = path.join(__dirname,'../../utils/testdata.json')
+    const jsondata = fs.readFileSync(filePath,'utf8')
+    gendata = JSON.parse(jsondata)
+    return gendata;
+}
+
+module.exports={genericdata}
