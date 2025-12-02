98a1732b3055d1ecbbafc6e65270ee170c604d57
SwapR1988 <swapnendras@gmail.com>
2025-12-02 12:21:44 +0530

    Refactor cucumber.js configuration format

--- cucumber.js
+++ cucumber.js
@@ -1,10 +1,12 @@
 module.exports = {
-  default: [
-    "features/**/*.feature",
-    "--require features/step_definitions/**/*.js",
-    "--require features/support/**/*.js",
-    "--timeout 60000",
-    "--format progress",
-    "--publish-quiet"
-  ].join(" ")
+  default: {
+    paths: ["features/**/*.feature"],
+    require: [
+      "features/step_definitions/**/*.js",
+      "features/support/**/*.js"
+    ],
+    format: ["progress"],
+    publishQuiet: true,
+    timeout: 60000
+  }
 };
