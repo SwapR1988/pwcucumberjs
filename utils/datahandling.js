2d4613fa09cb5fdb0e27fcb6f67ba834b607481d
SwapR1988 <swapnendras@gmail.com>
2025-12-03 12:07:21 +0530

    datahandling

--- utils/datahandling.js
+++ utils/datahandling.js
@@ -1,11 +1,43 @@
-const {genericData} = require('../features/support/hooks.js')
+const {genericdata} = require('./hooks');
 
-class datahandling{
+let testJsonData;
+let data;
 
-    async readdata(testdata){
-    const storedata = await genericData();
-    const splitdata = testdata.split("#")
+class DataHandling{
+
+    async readData(testData){ //function to read data from testData.json Student#TestCase1
+        try{
+            
+            testJsonData = await getGenericData(); //fetching json data from json
+        
+        //Splitting the testData string into array to fetch data from json
+        let testDataArray = testData.split('#'); 
+        const moduleData = testDataArray[0]; //Student
+        const testCase = testDataArray[1]; //TestCase1
+        console.log(`Module Name: ${moduleData}`)
+        console.log(`Test Case Name: ${testCase}`)
+
+        //Fetching data object using above array parameters
+        data = testJsonData[moduleData][0][testCase]
+        return data;
+    }
+    catch(error){
+        console.error("Exception Cached !!", error);
+    }
+
+    }
+
+    async getData(key){ //function to retrive value of a key from an object
+        try{
+            console.log(`Got data from dataset for ${key} : ${data[key]} `)
+            const requiredData = data[key];
+            return requiredData;
+        }
+        catch(error){
+        console.error("Exception Cached !!", error);
+    }
     }
 }
-
-module.exports = {datahandling}
+module.exports={
+    DataHandling
+}
